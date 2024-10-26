d01478fb8293f672c5b4b13112da43e27920b366
Nᴏᴛʜɪɴɢ... <125535875+nothing022@users.noreply.github.com>
2024-10-27 00:00:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,10 +14,10 @@ class Config(object):
     ARQ_API_KEY = "TBPYLF-SIOYFX-JALTSV-QEAMXE-ARQ"
     SPAMWATCH_API = "t9HHtrsmy7faPQWloX8xCvdZK~puDP2RnHLpb~qijQqDj94mhcMQdDP_xO0a_Iwe"
     TOKEN = getenv("TOKEN")
-    OWNER_ID = int(getenv("OWNER_ID", 5938660179))
-    OWNER_USERNAME = getenv("OWNER_USERNAME", "JARVIS_V2")
-    SUPPORT_CHAT = getenv("SUPPORT_CHAT", "Dora_Hub")
-    LOGGER_ID = int(getenv("LOGGER_ID", "-1002014167331"))
+    OWNER_ID = int(getenv("OWNER_ID", 1666243723))
+    OWNER_USERNAME = getenv("OWNER_USERNAME", "Tp_Editz")
+    SUPPORT_CHAT = getenv("SUPPORT_CHAT", "Idsp_bots")
+    LOGGER_ID = int(getenv("LOGGER_ID", "-1001863711542"))
     MONGO_URI = getenv("MONGO_DB_URI")
     DB_NAME = getenv("DB_NAME", "AvaRobot")
     REDIS_URL = "redis://default:wK6ZCiclq4iQKYpgfY90v6kd6WdPfEwl@redis-10186.c263.us-east-1-2.ec2.cloud.redislabs.com:10186/default"
